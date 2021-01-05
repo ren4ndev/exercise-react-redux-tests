e7f858454db6d029d3acfeb52c16c3ca32d02ded
Renan Oliveira <61306294+ren4ndev@users.noreply.github.com>
2021-01-05 09:21:27 -0300

    Estrutura inicial Estrutura inicial da aplicação para a realização dos exercícios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import { Provider } from "react-redux";
+import store from "./store";
+import ButtonClicks from "./ButtonClicks";
+import NumberClicks from "./NumberClicks";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <ButtonClicks />
+          <NumberClicks />
+        </Provider>
+      </div>
+    );
+  }
 }
+export default App;
