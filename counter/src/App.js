cb90d2dc06ff7d042351f561193f5b1f691b3b90
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-01-05 15:30:48 -0300

    Alterando local do Provider

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,17 +1,13 @@
 // src/App.js
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
 import ButtonClicks from './ButtonClicks';
 import NumberClicks from './NumberClicks';
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Provider store={store}>
           <ButtonClicks />
           <NumberClicks />
-        </Provider>
       </div>
     );
   }
